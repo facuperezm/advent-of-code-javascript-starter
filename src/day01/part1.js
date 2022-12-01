4cf947d0c6f4377aa1739f648c065403e52273c2
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2022-12-01 11:07:02 -0300

    day01

--- src/day01/part1.js
+++ src/day01/part1.js
@@ -1,5 +1,15 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input) {
-  return 0;
+  const info = input
+    .split("\n\n")
+    .map((x) =>
+      x
+        .split("\n")
+        .map(Number)
+        .reduce((a, b) => a + b)
+    )
+    .sort((a, b) => b - a);
+
+  return info[0];
 }
--- src/day01/part2.js
+++ src/day01/part2.js
@@ -1,5 +1,16 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(input) {
-  return 0;
+  const info = input
+    .split("\n\n")
+    .map((x) =>
+      x
+        .split("\n")
+        .map(Number)
+        .reduce((a, b) => a + b)
+    )
+    .sort((a, b) => b - a);
+
+  const sum = info[0] + info[1] + info[2];
+  return sum;
 }
